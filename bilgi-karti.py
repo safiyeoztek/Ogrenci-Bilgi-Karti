9b745587c44057454a7b4dccc81d9d1d566364fa
safiyeoztek <safiyeoztek47@gmail.com>
2026-03-22 20:59:31 +0300

    python dosyaları eklendi

---
+++ bilgi-karti.py
@@ -0,0 +1,8 @@
+print("----- KULLANICI BİLGİ KARTI -----")
+
+isim = "Safiye"
+yas = 19
+favoriSayi = 2
+yazilima_yenimi = False
+
+print("İsim: " + isim + "\n" + "Yaş: " + str(yas) + "\n" + "Favori Sayı: " + str(favoriSayi) + "\n" + "Yazılıma Yeni mi?: " + str(yazilima_yenimi))
